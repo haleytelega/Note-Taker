bffa36b8bb5ae25ba21ee3ec8b8260e0372771b7
Haley Telega <haleytelega@outlook.com>
2022-05-04 13:59:44 -0400

    trying to get api routes to work

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { notes } = require('../../db/db.json');
+
+
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+module.exports = router;
